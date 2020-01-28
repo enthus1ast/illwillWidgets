a8ed93038e5443f19ca3044adedbc91f9cdb34a2
David Krause <david@code0.xyz>
2020-01-28 18:06:01 +0100

    Signed-off-by: David Krause <david@code0.xyz>

--- button.nim
+++ button.nim
@@ -33,12 +33,27 @@ type
     text: string
     checked: bool
     color: ForegroundColor
+    textChecked: string
+    textUnchecked: string
+  # RadioButton = object of Checkbox
+  #   linked: set[RadioButton]
   InfoBox = object of Widget
     text: string
     color: ForegroundColor
     bgcolor: BackgroundColor
     w: int
-  # RadioButtonGroup = object
+  ChooseBox = object of Widget
+    color: ForegroundColor
+    bgcolor: BackgroundColor
+    bgcolorChoosen: BackgroundColor
+    choosenidx: int
+    w: int
+    h: int
+    elements: seq[string]
+
+# proc newRadioButtinGroup(radioButtons: seq[RadioButton]) =
+#   for radioButton in radioButton:
+#     radioButton.linked = radioButtons
   # RadioButton = object of Widget
   #   text: string
   #   checked: bool
@@ -56,6 +71,10 @@ type
 
 #   )
 
+#########################################################################################################
+# InfoBox
+#########################################################################################################
+
 proc newInfoBox(text: string, x, y: int, w = 10, color = fgBlack, bgcolor = bgWhite): InfoBox =
   result = InfoBox(
     text: text,
@@ -65,7 +84,6 @@ proc newInfoBox(text: string, x, y: int, w = 10, color = fgBlack, bgcolor = bgWh
     color: color,
     bgcolor: bgcolor,
   )
-
 
 proc render(tb: var TerminalBuffer, wid: InfoBox) =
   let oldFg = tb.getForegroundColor
@@ -83,20 +101,54 @@ proc dispatch(tb: var TerminalBuffer, wid: InfoBox, mi: MouseInfo): Events {.dis
   if wid.inside(mi) and mi.action == Pressed: result.incl MouseDown
   if wid.inside(mi) and mi.action == Released: result.incl MouseUp
 
+
+#########################################################################################################
+# Checkbox
+#########################################################################################################
+
 proc newCheckbox(text: string, x, y: int, color = fgBlue): Checkbox =
   result = Checkbox(
     text: text,
     x: x,
     y: y,
-    color: color
+    color: color,
+    textChecked: "[X] ",
+    textUnchecked: "[ ] "
   )
 
 proc render(tb: var TerminalBuffer, wid: Checkbox) =
   let oldFg = tb.getForegroundColor
   tb.setForegroundColor wid.color
-  let check = if wid.checked: "[X] " else: "[ ] "
+  let check = if wid.checked: wid.textChecked else: wid.textUnchecked
   tb.write(wid.x, wid.y, check & wid.text)
   tb.setForegroundColor oldFg
+
+proc inside(wid: Checkbox, mi: MouseInfo): bool =
+  return (mi.x in wid.x .. wid.x+wid.text.len + 3) and (mi.y == wid.y)
+
+proc dispatch(tr: var TerminalBuffer, wid: var Checkbox, mi: MouseInfo): Events {.discardable.} = 
+  ## if the mouse clicks this button 
+  # result = false
+  if wid.inside(mi) and mi.action == Pressed:
+    result.incl MouseDown
+  if wid.inside(mi) and mi.action == Released:
+    wid.checked = not wid.checked
+    result.incl MouseUp
+
+#########################################################################################################
+# RadioBox
+#########################################################################################################
+
+proc newRadioBox(text: string, x, y: int, color = fgBlue): Checkbox =
+  ## Radio box is actually a checkbox, you need to add the checkbox to a radio button group
+  result = newCheckbox(text, x, y, color)
+  result.textChecked = "(X) "
+  result.textUnchecked = "( ) "
+
+
+#########################################################################################################
+# Button
+#########################################################################################################
 
 proc newButton(text: string, x, y, w, h: int, color = fgBlue): Button = #, cb = ButtonCallback): Button = 
   result = Button(
@@ -107,7 +159,6 @@ proc newButton(text: string, x, y, w, h: int, color = fgBlue): Button = #, cb = 
     w: w,
     h: h,
     color: color,
-    # cb: cb
   )
 
 proc render(tb: var TerminalBuffer, wid: Button) =
@@ -128,25 +179,11 @@ proc render(tb: var TerminalBuffer, wid: Button) =
   )
   tb.setForegroundColor oldFg
 
-proc inside(wid: Checkbox, mi: MouseInfo): bool =
-  return (mi.x in wid.x .. wid.x+wid.text.len + 3) and (mi.y == wid.y)
-
-proc dispatch(tr: var TerminalBuffer, wid: var Checkbox, mi: MouseInfo): Events {.discardable.} = 
-  ## if the mouse clicks this button 
-  # result = false
-  if wid.inside(mi) and mi.action == Pressed:
-    result.incl MouseDown
-  if wid.inside(mi) and mi.action == Released:
-    wid.checked = not wid.checked
-    result.incl MouseUp
-  # result = wid.checked
-
 proc inside(wid: Button, mi: MouseInfo): bool =
   return (mi.x in wid.x .. wid.x+wid.w) and (mi.y in wid.y .. wid.y+wid.h)
 
 proc dispatch(tr: var TerminalBuffer, wid: var Button, mi: MouseInfo): Events {.discardable.} = 
   ## if the mouse clicks this button 
-  # if mi.action == Released: wid.pressed = false
   result = {}
   if not wid.inside(mi): 
     wid.pressed = false
@@ -159,88 +196,169 @@ proc dispatch(tr: var TerminalBuffer, wid: var Button, mi: MouseInfo): Events {.
     wid.pressed = false
     result.incl MouseUp
 
+#########################################################################################################
+# ChooseBox
+#########################################################################################################
 
-proc doClear(tb: var TerminalBuffer): proc() =
-  result = proc() {.gcsafe.} =
-    tb.clear()
+proc newChooseBox(elements: seq[string], x, y, w, h: int, color = fgBlue, label = "", choosenidx = 0): ChooseBox = #, cb = ButtonCallback): Button = 
+  result = ChooseBox(
+    # label: label,
+    elements: elements,
+    choosenidx: choosenidx,
+    x: x,
+    y: y,
+    w: w,
+    h: h,
+    color: color,
+  )
 
-proc cl(tb: var TerminalBuffer) =
-    tb.clear()
+proc choosenElement(wid: ChooseBox): string =
+  return wid.elements[wid.choosenidx]
 
-var btnTest = newButton("test", 38, 3, 15, 2) #, cb = proc () = echo "FOO")
-# var btnTest = newButton("test", 38, 3, 15, 2, cb = proc (tr: TerminalBuffer) = discard )
-var btnClear = newButton("clear", 38, 6, 15, 2) #, cb = cl)
-var chkTest = newCheckbox("Some content", 38, 9) #, cb = cl)
-var chkTest2 = newCheckbox("Some more content", 38, 10) #, cb = cl)
-var infoBox = newInfoBox("",0 ,0, terminalWidth())
+template setWidgetColor(tb: var TerminalBuffer, wid: typed) =
+  tb.setForegroundColor wid.color
+  tb.setBackgroundColor wid.bgcolor
 
-# var widgets = @[
-#   btnTest,
-#   btnClear,
-#   # chkTest
+proc render(tb: var TerminalBuffer, wid: ChooseBox) =
+  let oldFg = tb.getForegroundColor
+  tb.setForegroundColor wid.color
+  tb.fill(wid.x, wid.y, wid.x+wid.w, wid.y+wid.h)
+  for idx, elemRaw in wid.elements:
+    let elem = elemRaw.alignLeft(wid.w)
+    if idx == wid.choosenidx:
+      tb.write resetStyle
+      tb.write(wid.x+1, wid.y+ 1 + idx, wid.color, wid.bgcolor, styleReverse, elem)
+    else:
+      tb.write resetStyle
+      tb.write(wid.x+1, wid.y+ 1 + idx, wid.color, wid.bgcolor, elem)
+  tb.write resetStyle
+  tb.setWidgetColor(wid)
+  tb.drawRect(
+    wid.x, 
+    wid.y,
+    wid.x + wid.w, 
+    wid.y + wid.h, 
+    # doubleStyle=wid.pressed, 
+  )
+  tb.setForegroundColor oldFg
+
+proc inside(wid: ChooseBox, mi: MouseInfo): bool =
+  return (mi.x in wid.x .. wid.x+wid.w) and (mi.y in wid.y .. wid.y+wid.h)
+
+proc dispatch(tr: var TerminalBuffer, wid: var ChooseBox, mi: MouseInfo): Events {.discardable.} = 
+  ## if the mouse clicks this button 
+  result = {}
+  if not wid.inside(mi): 
+    # wid.pressed = false
+    return
+  case mi.action
+  of Pressed:
+    result.incl MouseDown
+  of Released:
+    wid.choosenidx = clamp( (mi.y - wid.y)-1 , 0, wid.elements.len-1)
+    result.incl MouseUp  
+
+
+when isMainModule:
+  import strformat
+
+  var btnTest = newButton("fill", 38, 3, 15, 2)
+  var btnClear = newButton("clear", 38, 6, 15, 2)
+  var chkTest = newCheckbox("Some content", 38, 9)
+  var chkTest2 = newCheckbox("Fill Color", 38, 10)
+  chkTest2.textUnchecked = ":)  "
+  chkTest2.textChecked =   ":(  "
+
+  var chkDraw = newCheckbox("Draw?", 38, 11)
   
-# ]
-# echo button
-# if true: quit()
-# tb.write("foo")
-while true:
-  # tb.drawRect(38, 3, 50, 5, doubleStyle=false)
-  # tb.write(bb)
-  var key = getKey()
-  case key
-  of Key.None: discard
-  of Key.Escape, Key.Q: exitProc()
-  of Key.Mouse:
-    let coords = getMouse()
-    # echo coords
-    var ev: Events
+  # Info box
+  var infoBox = newInfoBox("",0 ,0, terminalWidth())
+  var infoBoxMouse = newInfoBox("",0 ,0, terminalWidth())
 
-    ev = tb.dispatch(btnClear, coords)
-    if ev.contains MouseUp:
-      tb.clear()
-    if ev.contains MouseDown:
-      infoBox.text = "CLEARS THE SCREEN!"
-    
-    ev = tb.dispatch(btnTest, coords)
-    if ev.contains MouseUp:
-      tb.clear("#")
-    if ev.contains MouseDown:
-      infoBox.text = "Fills the screen!!"
+  # Radio buttons
+  var chkRadA = newRadioBox("OptionA", 56, 4)
+  var chkRadB = newRadioBox("OptionB", 56, 5)
+  var chkRadC = newRadioBox("OptionC", 56, 6)
 
-    if tb.dispatch(chkTest, coords).contains MouseUp:
-      infoBox.text = "chk test is: " & $chkTest.checked
-    
-    if tb.dispatch(chkTest2, coords).contains MouseUp:
-      if chkTest2.checked:
-        echo "red"
-        tb.setForegroundColor fgRed
-      else:
-        echo "green"
-        tb.setForegroundColor fgGreen
+  var chooseBox = newChooseBox(@[" ", "#", "@", "§"], 70, 3, 10, 5, choosenidx=2)
 
-    ev = tb.dispatch(infoBox, coords)
-    if ev.contains MouseDown:
-      infoBox.text &= "#"
-    if ev.contains MouseUp:
-      infoBox.text &= "^"
-            
-    
-    # if coords.action == MouseButtonAction.Pressed:
-    #   tb.write coords.x, coords.y, fgRed, "♥"
-    # else:
-    #   tb.write coords.x, coords.y, fgGreen, "⌀"
-  else:
-    echo key
-    discard
+  while true:
+    var key = getKey()
+    case key
+    of Key.None: discard
+    of Key.Escape, Key.Q: exitProc()
+    of Key.Mouse:
+      let coords = getMouse()
+      infoBoxMouse.text = $coords
+      var ev: Events
+
+      ev = tb.dispatch(btnClear, coords)
+      if ev.contains MouseUp:
+        tb.clear()
+      if ev.contains MouseDown:
+        infoBox.text = "CLEARS THE SCREEN!"
+      
+      ev = tb.dispatch(btnTest, coords)
+      if ev.contains MouseUp:
+        tb.clear(chooseBox.choosenElement)
+      if ev.contains MouseDown:
+        infoBox.text = "Fills the screen!!"
+
+      if tb.dispatch(chkTest, coords).contains MouseUp:
+        infoBox.text = "chk test is: " & $chkTest.checked
+      
+      if tb.dispatch(chkTest2, coords).contains MouseUp:
+        if chkTest2.checked:
+          infoBox.text = "red"
+          tb.setForegroundColor fgRed
+        else:
+          infoBox.text = "green"
+          tb.setForegroundColor fgGreen
+
+      ev = tb.dispatch(chkDraw, coords)
+      if ev.contains MouseDown:
+        infoBox.text = "Enables/Disables drawing"
+
+      ev = tb.dispatch(infoBox, coords)
+      if ev.contains MouseDown:
+        infoBox.text &= "#"
+      if ev.contains MouseUp:
+        infoBox.text &= "^"
+
+      ev = tb.dispatch(chooseBox, coords)
+      if ev.contains MouseUp:
+        infoBox.text = fmt"Choose box choosenidx: {chooseBox.choosenidx} -> {chooseBox.choosenElement()}"
+      
   
-  tb.render(btnClear)
-  tb.render(btnTest)
-  tb.render(chkTest)
-  tb.render(chkTest2)
+      if chkDraw.checked:
+        if coords.action == MouseButtonAction.Pressed:
+          tb.write coords.x, coords.y, fgRed, "♥"
+        else:
+          tb.write coords.x, coords.y, fgGreen, "⌀"
+        
+    else:
+      echo key
+      discard
+    
+    tb.render(btnClear)
+    tb.render(btnTest)
+    tb.render(chkTest)
+    tb.render(chkTest2)
+    tb.render(chkDraw)
 
-  infoBox.w = terminalWidth()
-  infoBox.y = terminalHeight()-1
-  tb.render(infoBox)
+    tb.render(chkRadA)
+    tb.render(chkRadB)
+    tb.render(chkRadC)
 
-  tb.display()
-  sleep(20)
+    # Update the info box to always be on the bottom
+    infoBox.w = terminalWidth()
+    infoBox.y = terminalHeight()-1
+    tb.render(infoBox)
+    
+    # No need to update
+    tb.render(infoBoxMouse)
+
+    tb.render(chooseBox)
+
+    tb.display()
+    sleep(20)
